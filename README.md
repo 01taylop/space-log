febf5520f9c91b38e7fe39bdbb98fb7507c6b2c6
Patrick Taylor <pattaylor@expediagroup.com>
2022-05-09 14:56:12 +0100

    Created Space Log 🪐

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A simple utility for logging tabular data to the terminal.
 
 ---
+
+## TODO
+
+[ ] Docs
+[ ] Unit Tests
---
+++ src/index.mjs
@@ -0,0 +1,46 @@
+import chalk from 'chalk'
+
+const log = (config, data) => {
+  const { columnKeys, headings } = config
+
+  // Intentional Spacing
+  console.log()
+
+  // Calculate Column Width
+  const columnWidths = {}
+
+  columnKeys.forEach((key, index) => {
+    const headingLength = headings[index].length
+    const dataLengths = data.map(item => item[key].length)
+
+    columnWidths[key] = Math.max(headingLength, ...dataLengths) + 1
+  })
+
+  // Log Headings
+  const headingLine = columnKeys.map((key, index) => {
+    const title = headings[index] || 'Unknown'
+    const spacing = columnWidths[key] - title.length
+    return `${chalk.underline(title)}${' '.repeat(spacing)}`
+  }).join('')
+  console.log(headingLine)
+
+  // Log Data
+  data.forEach(item => {
+    let line = ''
+    columnKeys.map(key => {
+      const text = item[key] || '-'
+      const spacing = columnWidths[key] - text.length
+      const theme = item[`${key}Theme`] || null
+      const styledText = theme ? theme(text) : text
+      line = `${line}${styledText}${' '.repeat(spacing)}`
+    })
+    console.log(line)
+  })
+
+  // Intentional Spacing
+  console.log()
+}
+
+export {
+  log,
+}
