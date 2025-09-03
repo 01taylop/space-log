d1d465c136f7b293960c931044903cbb9f07a4fb
Patrick Taylor <hello@patricktaylor.dev>
2025-09-03 22:36:51 +0100

    Repo maintenance and Node build compatibility update 🔧 (#14)
    * Ensure module builds targeting lowest supported Node version 🔧
    
    * Improved TypeScript definitions for better type safety 🆎
    
    * Simplified Jest config 🃏
    
    * Updated package.json ⚙️
    
    * Updated minor and patch dependencies 🆙
    
    * Upgraded `Jest` to major version 30 🆒
    
    * Updated GitHub workflows and templates 💞
    
    * Updated example screenshot 🖼️
    
    * Added `example` script to create the example on the Readme 🕹️

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,13 +1,35 @@
+const semver = require('semver')
+
+const { engines } = require('./package.json')
+
+const nodeVersion = semver.minVersion(engines.node)?.version
+
+if (!nodeVersion) {
+  throw new Error(`Invalid or missing engines.node in package.json: ${engines.node}`)
+}
+
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    ['@babel/preset-env', {
+      targets: { node: 'current' },
+    }],
   ],
   env: {
     cjs: {
-      presets: [["@babel/preset-env", { modules: "commonjs" }]],
+      presets: [
+        ['@babel/preset-env', {
+          modules: 'commonjs',
+          targets: { node: nodeVersion },
+        }],
+      ],
     },
     esm: {
-      presets: [["@babel/preset-env", { modules: false }]],
+      presets: [
+        ['@babel/preset-env', {
+          modules: false,
+          targets: { node: nodeVersion },
+        }],
+      ],
     },
   },
 }
---
+++ scripts/example.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+
+import { spaceLog } from '../src/index.js'
+
+spaceLog({
+  columnKeys: ['name', 'population', 'area'],
+  headings: ['Region', 'Population', 'Area (km²)'],
+}, [
+  { name: 'Hokkaidō', population: '5.4 million', area: '83,000', areaTheme: chalk.green },
+  { name: 'Tōhoku', population: '8.9 million', area: '67,000' },
+  { name: 'Kantō', population: '43.3 million', area: '32,000', populationTheme: chalk.green },
+  { name: 'Chūbu', population: '21.4 million', area: '67,000' },
+  { name: 'Kansai', population: '22.5 million', area: '33,000' },
+  { name: 'Chūgoku', population: '7.3 million', area: '32,000' },
+  { name: 'Shikoku', population: '3.8 million', area: '19,000' },
+  { name: 'Kyūshū & Okinawa', population: '14 million', area: '44,000' },
+])
